f04f52168c9e7883ab0458b850fb1ac1d55a8d4b
risarisarisa <from.aqua.to@gmail.com>
2021-06-20 14:35:29 +1000

    Added the screen of creating new task

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router';
 import HeaderComponent from "./components/HeaderComponent";
 import TaskListComponent from "./components/TaskListComponent";
 import TaskShowComponent from "./components/TaskShowComponent";
+import TaskCreateComponent from "./components/TaskCreateComponent";
 
 require('./bootstrap');
 
@@ -16,6 +17,11 @@ const router = new VueRouter({
       path: '/tasks',
       name: 'task.list',
       component: TaskListComponent
+    },
+    {
+      path: '/tasks/:create',
+      name: 'task.create',
+      component: TaskCreateComponent,
     },
     {
       path: '/tasks/:taskId',
