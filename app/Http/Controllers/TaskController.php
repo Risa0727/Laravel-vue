be1e735ea290928d29d2cb404c4ef05b47b1c543
risarisarisa <from.aqua.to@gmail.com>
2021-06-23 14:29:39 +1000

    API: update and delete

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -21,4 +21,15 @@ class TaskController extends Controller
   public function store(Request $request) {
     return Task::create($request->all());
   }
+
+  public function update(Request $request, Task $task) {
+    $task->update($request->all());
+    return $task;
+  }
+
+  public function destroy(Task $task) {
+    $task->delete();
+
+    return $task;
+  }
 }
