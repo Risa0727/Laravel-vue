82cf876a61f0af762fb08ee8a9274f17934aa8b0
risarisarisa <from.aqua.to@gmail.com>
2021-06-23 21:40:01 +1000

    updated task detail page

--- resources/js/components/TaskShowComponent.vue
+++ resources/js/components/TaskShowComponent.vue
@@ -6,22 +6,22 @@
           <div class="form-group row border-bottom">
             <label for="id" class="col-sm-3 col-form-label">ID</label>
             <input type="text" class="col-sm-9 form-control=plaintext"
-              readonly id="id" v-bind:value="taskId">
+              readonly id="id" v-model="task.id">
           </div>
           <div class="form-group row border-bottom">
             <label for="title" class="col-sm-3 col-form-label">Tile</label>
             <input type="text" class="col-sm-9 form-control-plaintext"
-              readonly id="title" value="title title">
+              readonly id="title" v-model="task.title">
           </div>
           <div class="form-group row border-bottom">
             <label for="content" class="col-sm-3 col-form-label">Content</label>
             <input type="text" class="col-sm-9 form-control-plaintext"
-              readyonly id="content" value="content content">
+              readyonly id="content" v-model="task.content">
           </div>
           <div class="form-group row border-bottom">
             <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
             <input type="text" class="col-sm-9 form-control-plaintext"
-              readonly id="person-in-charge" value="Ichiro">
+              readonly id="person-in-charge" v-model="task.person_in_charge">
           </div>
         </form>
       </div>
@@ -33,7 +33,24 @@
 <script>
   export default {
     props: {
-      taskId: String
+      taskId: Number
+    },
+    data: function () {
+      return {
+        task: {}
+      }
+    },
+    methods: {
+      getTask() {
+        axios.get('/00/laravel-vue/api/tasks/' + this.taskId)
+          .then((res) => {
+            this.task = res.data;
+          });
+      }
+    },
+    mounted() {
+      this.getTask();
+      console.log(this.$route.path);
     }
   }
 </script>
